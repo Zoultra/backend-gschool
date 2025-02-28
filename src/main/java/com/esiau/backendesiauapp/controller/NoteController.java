2c642f65bfb2b34f34830291827e955a652acc11
ZOUMANA TRAORE <49890652+Zoultra@users.noreply.github.com>
2025-02-28 03:12:52 +0000

    Update NoteController.java

--- src/main/java/com/esiau/backendesiauapp/controller/NoteController.java
+++ src/main/java/com/esiau/backendesiauapp/controller/NoteController.java
@@ -23,7 +23,7 @@ import com.esiau.backendesiauapp.services.NoteService;
 
 @RestController
 
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins={"http://localhost:4200", "https://esiauwebapp.vercel.app"})
 @RequestMapping("/backendesiauapp/v1/")
 public class NoteController {
     
