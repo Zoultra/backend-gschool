b4e8498051556e5c7adee9a2a98af7527dcee5de
ZOUMANA TRAORE <49890652+Zoultra@users.noreply.github.com>
2025-02-28 03:13:28 +0000

    Update PaiementController.java

--- src/main/java/com/esiau/backendesiauapp/controller/PaiementController.java
+++ src/main/java/com/esiau/backendesiauapp/controller/PaiementController.java
@@ -21,7 +21,7 @@ import com.esiau.backendesiauapp.services.PaiementService;
 
 @RestController
 
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins={"http://localhost:4200", "https://esiauwebapp.vercel.app"})
 @RequestMapping("/backendesiauapp/v1/")
 public class PaiementController {
 	
