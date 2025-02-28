1bc38a052ea541622a699eab50ca0fb7dc703a6f
ZOUMANA TRAORE <49890652+Zoultra@users.noreply.github.com>
2025-02-28 03:14:29 +0000

    Update ProfesseurController.java

--- src/main/java/com/esiau/backendesiauapp/controller/ProfesseurController.java
+++ src/main/java/com/esiau/backendesiauapp/controller/ProfesseurController.java
@@ -20,7 +20,7 @@ import com.esiau.backendesiauapp.services.ProfesseurService;
 
 @RestController
 
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins={"http://localhost:4200", "https://esiauwebapp.vercel.app"})
 @RequestMapping("/backendesiauapp/v1/")
 
 public class ProfesseurController {
