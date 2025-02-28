7f6cb60c7231c85071781b88c199e4166d2ef9af
ZOUMANA TRAORE <49890652+Zoultra@users.noreply.github.com>
2025-02-28 03:01:03 +0000

    Update UserController.java

--- src/main/java/com/esiau/backendesiauapp/controller/UserController.java
+++ src/main/java/com/esiau/backendesiauapp/controller/UserController.java
@@ -18,7 +18,7 @@ import com.esiau.backendesiauapp.services.UserService;
 
 @RestController
 
-@CrossOrigin(origins={"http://localhost:4200", "http://localhost:58227"})
+@CrossOrigin(origins={"http://localhost:4200", "https://esiauwebapp.vercel.app"})
 @RequestMapping("/backendesiauapp/v1/")
 public class UserController {
     
