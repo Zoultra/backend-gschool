6e9c7d8fcca7ef91b4a5111438d3bd8967fbc00a
ZOUMANA TRAORE <49890652+Zoultra@users.noreply.github.com>
2025-02-28 03:10:48 +0000

    Update EtudiantController.java

--- src/main/java/com/esiau/backendesiauapp/controller/EtudiantController.java
+++ src/main/java/com/esiau/backendesiauapp/controller/EtudiantController.java
@@ -24,7 +24,7 @@ import com.esiau.backendesiauapp.services.NiveauService;
 
 @RestController
 
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins={"http://localhost:4200", "https://esiauwebapp.vercel.app"})
 @RequestMapping("/backendesiauapp/v1/")
 public class EtudiantController {
  
